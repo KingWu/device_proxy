8be844ffc634d39b446b234e0f81a433e886cf9e
yusuf <yusuf@setel.my>
2021-07-15 12:06:07 +0800

    chore: fix type

--- lib/src/device_proxy.dart
+++ lib/src/device_proxy.dart
@@ -7,8 +7,9 @@ class DeviceProxy {
   static const MethodChannel _channel =
       const MethodChannel('com.intechlab/device_proxy');
 
-  static Future<ProxyConfig> get proxyConfig async {
-    final String proxyData = await (_channel.invokeMethod('getProxySetting') as FutureOr<String>);
+  static Future<ProxyConfig?> get proxyConfig async {
+    final String? proxyData = await _channel.invokeMethod<String>('getProxySetting');
+    if (proxyData == null) return null;
     return ProxyConfig(proxyData);
   }
 }
